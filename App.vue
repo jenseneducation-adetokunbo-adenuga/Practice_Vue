dba6a8fba6ac12bf031942a8624ec6d2dd7f268e
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-02-14 19:07:25 +0100

    Beginning list

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="app">
+    <p v-on:click="changeText"> {{message}} </p>
+    <ul>
+        <li v-for="fruit in fruits" 
+        v-bind:key="fruit"
+        v-on:click="addFruit(fruit)"
+        >{{fruit.text}}</li>
+    </ul>
+</div> 
+</template>
+
+<script>
+export default {
+    data: () => ({
+        message: "Fruit!",
+        fruits: [
+            {text:"Banana"},
+            {text:"Kiwi"}, 
+            {text:"Carrot"},
+            {text:"Orange"},
+            {text:"Apple"}        
+        ],
+        sallad : []
+    }),
+
+    methods: {
+        changeText(){            
+            this.message = "GrillKorv"
+        }
+    }
+}
+</script>
+
+<!--<style>
+#app{
+    background-color: hotpink;
+}
+</style>-->
